970559ae9f7467c8c946b2d7d068e81f585b9f31
Emilio <ediaz-goico@me.bergen.edu>
2020-05-07 11:47:14 -0400

    finishing touches

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,16 @@
+import { useEffect } from "react"
+import { useLocalStorage } from "./useLocalStorage"
+
+
+export const useDarkMode = (key, initialValue) => {
+    const [value, setValue] = useLocalStorage(key, initialValue)
+    const body = document.querySelector("body")
+    useEffect(()=> {
+        if(value === true) {
+            body.classList.add("dark-mode")
+        } else if (value === false) {
+            body.classList.remove("dark-mode")
+        }
+    }, [value, body.classList])
+    return [value, setValue]
+} 
