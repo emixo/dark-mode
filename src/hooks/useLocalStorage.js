318184735d5e3cafd0a6b2eeab08521d366a28e2
Emilio <ediaz-goico@me.bergen.edu>
2020-05-07 11:31:22 -0400

    finished mvp

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+//key will be a string
+//value can be anything, object, array, string, number, boolean, etc
+export const useLocalStorage = (key, initialValue) => {
+    const [storedValue, setStoredValue] = useState(() => {
+        const localPair = window.localStorage.getItem(key)
+        return localPair ? JSON.parse(localPair) : initialValue
+    }); //state
+
+  setValue = value => {
+    setStoredValue(value)
+    window.localStorage.setItem(key, JSON.stringify(value))
+  } // setter function 
+
+  return [storedValue, setValue]
+}; 
